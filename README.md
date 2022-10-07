1072e07bf25fca779c2bd4fb40f3112f33a3fc7f
Pekka Poukamo <p.poukamo@gmail.com>
2022-10-07 12:26:14 +0300

    Show history

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Starting point tutorial: https://www.codeinwp.com/blog/how-to-write-a-chrome-extension/
+
+## Todo
+
+- Store data
+- Display stored data
+- Wake up pages
--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('success!')
-
 const WEEKDAYS = {
 	'MON': 1,
 	'TUE': 2,
@@ -26,18 +24,72 @@ const getNextWeekdayFromDate = (date, weekday) => {
 
 const getNextWeekdaysFromToday = () => Object.keys(WEEKDAYS).map(weekday => getNextWeekdayFromDate(new Date(), WEEKDAYS[weekday]))
 
-document.addEventListener("DOMContentLoaded", () => {
+const initializeWeekDayButtons = () => {
+	document.querySelector('#buttons').innerHTML = ''
+
+	const weekDayFormatOptions = {weekday: 'long'}
+	const dateFormatOptions = {day: 'numeric', month: 'short'}
+
 	getNextWeekdaysFromToday().map(weekday => {
 		const button = document.querySelector('#datebutton').content.cloneNode(true)
-		const weekDayFormatOptions = {weekday: 'long'}
-		const dateFormatOptions = {day: 'numeric', month: 'short'}
 
 		button.querySelector('.datebutton__weekday').textContent = new Intl.DateTimeFormat('en-US', weekDayFormatOptions).format(weekday)
 		button.querySelector('.datebutton__date').textContent =  new Intl.DateTimeFormat('en-US', dateFormatOptions).format(weekday)
 		button.children[0].setAttribute('value', weekday.toISOString())
 
 		return button
-	}).forEach(button => {
-		document.querySelector('#buttons').appendChild(button)
+	}).forEach(button => document.querySelector('#buttons').appendChild(button))
+}
+
+const initializeHistory = () => {
+	chrome.storage.local.get({'snoozedPages': []}, result => {
+		const pageLinksElement = document.querySelector('#page-links')
+		pageLinksElement.innerHTML = ''
+
+		if (result.snoozedPages.length === 0) {
+			pageLinksElement.innerHTML = 'No snoozed pages yet.'
+		}
+		result.snoozedPages.map(page => {
+			const pageLink = document.querySelector('#page-link-template').content.cloneNode(true)
+
+			pageLink.querySelector('.page-link__url').href = page.url
+			pageLink.querySelector('.page-link__title').textContent = page.title
+			pageLink.querySelector('.page-link__wakeupdate').textContent = page.wakeUpDate
+			return pageLink
+		}).forEach(pageLink => pageLinksElement.appendChild(pageLink))
 	})
+}
+
+
+const snoozePage = (url, title, wakeUpDate) => {
+	chrome.storage.local.get({'snoozedPages': []}, (result) => {
+		const snoozedPages = result.snoozedPages
+
+		snoozedPages.push({
+			id: Math.random(),
+			wakeUpDate: wakeUpDate.toISOString(),
+			url, title, 
+		})
+
+		chrome.storage.local.set({'snoozedPages': snoozedPages})
+	})
+}
+
+const clearSnoozedPages = () => chrome.storage.local.set({'snoozedPages': []})
+
+
+document.addEventListener("DOMContentLoaded", () => {
+	// initialize main popup page
+	if (window.location.href.match('popup.html')) {
+		initializeWeekDayButtons()
+	}
+
+	if (window.location.href.match('history.html')) {
+		initializeHistory()
+
+		document.querySelector('#clear-button').addEventListener('click', () => {
+			clearSnoozedPages()
+			initializeHistory()
+		})
+	}
 })
