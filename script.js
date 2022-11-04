a563045f9ccf7f03b797ddaa2f03b66142b7563e
Pekka Poukamo <p.poukamo@gmail.com>
2022-11-04 10:29:00 +0200

    Add single page wake up & organize code

--- script.js
+++ script.js
@@ -8,6 +8,173 @@ const WEEKDAYS = {
 	'SUN': 0,
 }
 
+/*
+	Main functions
+*/
+
+const openPageById = id => {
+	getSnoozedPages().then(snoozedPages => {
+		const page = snoozedPages.find(page => page.id === id);
+		chrome.tabs.create({
+			url: page.url
+		})
+	})
+}
+
+const snoozePages = (pages) => {
+	return getSnoozedPages().then(snoozedPages => {
+		pages.forEach(page => snoozedPages.push({
+			id: getUID(),
+			wakeUpDate: page.wakeUpDate.toISOString(),
+			title: page.title,
+			url: page.url,
+		}))
+
+		return setSnoozedPages(snoozedPages)
+	})
+}
+
+/*
+	Building the UI
+*/
+
+const initializeWeekDayButtons = () => {
+	const buttonContainer = document.querySelector('#buttons')
+	buttonContainer.innerHTML = ''
+
+	getNextWeekdaysFromToday()
+	.map(getSnoozeButton(document.querySelector('#datebutton').content))
+	.forEach(button => buttonContainer.appendChild(button))
+}
+
+const getSnoozeButton = buttonTemplate => weekday => {
+	const weekDayFormatOptions = {weekday: 'long'}
+	const dateFormatOptions = {day: 'numeric', month: 'short'}
+
+	const button = buttonTemplate.cloneNode(true)
+
+	button.querySelector('.datebutton__weekday').textContent = new Intl.DateTimeFormat('en-US', weekDayFormatOptions).format(weekday)
+	button.querySelector('.datebutton__date').textContent = new Intl.DateTimeFormat('en-US', dateFormatOptions).format(weekday)
+	button.children[0].setAttribute('value', weekday.toISOString())
+	// button.addEventListener('click', getSnoozeButtonFunction(weekday))
+
+	return button
+}
+
+const getSnoozeButtonFunction = date => async () => {
+	date.setHours(8, 0, 0, 0)
+
+	const tabs = await chrome.tabs.query({highlighted: true, currentWindow: true})
+	const pages = tabs.map(tab => ({
+		title: tab.title,
+		url: tab.url,
+		wakeUpDate: date,
+	}))
+	
+	snoozePages(pages)
+	chrome.tabs.remove(tabs.map(tab => tab.id))
+}
+
+const initializeHistory = () => {
+	getSnoozedPages()
+	.then(snoozedPages => {
+		const pageLinksElement = document.querySelector('#page-links')
+		pageLinksElement.innerHTML = ''
+
+		if (snoozedPages.length === 0) {
+			pageLinksElement.innerHTML = 'No snoozed pages yet.'
+		}
+
+		snoozedPages
+		.sort(byDate)
+		.filter(page => page.openedDate === undefined)
+		.map(getPageElement)
+		.forEach(pageLink => pageLinksElement.appendChild(pageLink))
+	})
+}
+
+const getPageElement = page => {
+	const pageLink = document.querySelector('#page-link-template').content.cloneNode(true)
+	const dateFormatOptions = {weekday: 'short', day: 'numeric', month: 'short', hour: 'numeric', minute: 'numeric', hour12: false}
+
+	pageLink.querySelector('.page-link__url').href = page.url
+	pageLink.querySelector('.page-link__title').textContent = page.title
+	pageLink.querySelector('.page-link__wakeupdate').textContent = new Intl.DateTimeFormat('en-US', dateFormatOptions).format(new Date(page.wakeUpDate))
+	pageLink.querySelector('.page-link__wakeup-button').addEventListener('click', getWakeupButtonFunction(page.id))
+	return pageLink
+}
+
+const getWakeupButtonFunction = id => async () => {
+	openPageById(id)
+	setPageValue(id, 'openedDate', (new Date().toISOString()))
+}
+
+/*
+	Storage access
+*/
+
+const clearSnoozedPages = () => chrome.storage.local.set({'snoozedPages': []})
+
+const getSnoozedPages = () => {
+	return new Promise((resolve, reject) => {
+		try {
+			chrome.storage.local.get({'snoozedPages': []}, result => {
+				if (chrome.runtime.lastError) {
+					reject(chrome.runtime.lastError)
+				}
+
+				resolve(result.snoozedPages)
+			})
+		} catch (error) {
+			reject(error)
+		}
+		
+	})
+	
+}
+
+const setSnoozedPages = snoozedPages => {
+	return new Promise((resolve, reject) => {
+		try {
+			chrome.storage.local.set({'snoozedPages': snoozedPages}, result => {
+				if (chrome.runtime.lastError) {
+					reject(chrome.runtime.lastError)
+				}
+
+				resolve(result)
+			})
+		} catch (error) {
+			reject(error)
+		}
+	})
+}
+
+const setPageValue = (id, field, value) => {
+	getSnoozedPages().then(snoozedPages => {
+		const page = snoozedPages.find(page => page.id === id);
+		page[field] = value
+
+		chrome.storage.local.set({'snoozedPages': snoozedPages})
+	})
+}
+
+/* 
+	Utilities
+*/
+
+const byDate = (page1, page2) => new Date(page1.wakeUpDate) - new Date(page2.wakeUpDate)
+
+// getUID function from https://dev.to/rahmanfadhil/how-to-generate-unique-id-in-javascript-1b13#comment-1ol48
+const getUID = () => String(
+	Date.now().toString(32) + Math.random().toString(16)
+	).replace(/\./g, '')
+
+
+const trace = string => object => {
+	console.log(string, object)
+	return object
+}
+
 const getNextWeekdayFromDate = (date, weekday) => {
 	const currentWeekday = date.getDay(); // 0 (Sun) - 6 (sat)
 	let daysToAdvance = (weekday - currentWeekday)
@@ -24,100 +191,17 @@ const getNextWeekdayFromDate = (date, weekday) => {
 
 const getNextWeekdaysFromToday = () => Object.keys(WEEKDAYS).map(weekday => getNextWeekdayFromDate(new Date(), WEEKDAYS[weekday]))
 
-const getClickFunction = date => async () => {
-	date.setHours(8, 0, 0, 0)
 
-	const tabs = await chrome.tabs.query({highlighted: true, currentWindow: true})
-	const pages = tabs.map(tab => ({
-		title: tab.title,
-		url: tab.url,
-		wakeUpDate: date,
-	}))
-	
-	snoozePages(pages)
-	chrome.tabs.remove(tabs.map(tab => tab.id))
-}
-
-
-const initializeWeekDayButtons = () => {
-	document.querySelector('#buttons').innerHTML = ''
-
-	const weekDayFormatOptions = {weekday: 'long'}
-	const dateFormatOptions = {day: 'numeric', month: 'short'}
-
-	getNextWeekdaysFromToday().map(weekday => {
-		const button = document.querySelector('#datebutton').content.cloneNode(true)
-
-		button.querySelector('.datebutton__weekday').textContent = new Intl.DateTimeFormat('en-US', weekDayFormatOptions).format(weekday)
-		button.querySelector('.datebutton__date').textContent = new Intl.DateTimeFormat('en-US', dateFormatOptions).format(weekday)
-		button.children[0].setAttribute('value', weekday.toISOString())
-		// button.addEventListener('click', getClickFunction(weekday))
-
-		return button
-	}).forEach(button => document.querySelector('#buttons').appendChild(button))
-}
-
-const initializeHistory = () => {
-	const dateFormatOptions = {weekday: 'short', day: 'numeric', month: 'short', hour: 'numeric', minute: 'numeric', hour12: false}
-
-	chrome.storage.local.get({'snoozedPages': []}, result => {
-		const pageLinksElement = document.querySelector('#page-links')
-		pageLinksElement.innerHTML = ''
-
-		if (result.snoozedPages.length === 0) {
-			pageLinksElement.innerHTML = 'No snoozed pages yet.'
-		}
-		result.snoozedPages
-		.sort(byDate)
-		.map(page => {
-			const pageLink = document.querySelector('#page-link-template').content.cloneNode(true)
-
-			pageLink.querySelector('.page-link__url').href = page.url
-			pageLink.querySelector('.page-link__title').textContent = page.title
-			pageLink.querySelector('.page-link__wakeupdate').textContent = new Intl.DateTimeFormat('en-US', dateFormatOptions).format(new Date(page.wakeUpDate))
-			return pageLink
-		}).forEach(pageLink => pageLinksElement.appendChild(pageLink))
-	})
-}
-
-// getUID function from https://dev.to/rahmanfadhil/how-to-generate-unique-id-in-javascript-1b13#comment-1ol48
-const getUID = () => String(
-    Date.now().toString(32) + Math.random().toString(16)
-  ).replace(/\./g, '')
-
-const byDate = (page1, page2) => {
-	console.log('sort', page1.wakeUpDate, page2.wakeUpDate, new Date(page1.wakeUpDate) - new Date(page2.wakeUpDate))
-	return new Date(page1.wakeUpDate) - new Date(page2.wakeUpDate)
-}
-
-
-const snoozePages = (pages) => {
-
-
-	chrome.storage.local.get({'snoozedPages': []}, (result) => {
-		const snoozedPages = result.snoozedPages
-
-		pages.forEach(page => snoozedPages.push({
-			id: getUID(),
-			wakeUpDate: page.wakeUpDate.toISOString(),
-			title: page.title,
-			url: page.url,
-		}))
-		
-
-		chrome.storage.local.set({'snoozedPages': snoozedPages})
-	})
-}
-
-const clearSnoozedPages = () => chrome.storage.local.set({'snoozedPages': []})
-
+/*
+	Run the extension
+*/
 
 document.addEventListener("DOMContentLoaded", () => {
 	// initialize main popup page
 	if (window.location.href.match('popup.html')) {
 		initializeWeekDayButtons()
 
-		Array.from(document.querySelector('#buttons').children).forEach(child => child.addEventListener('click', getClickFunction(new Date(child.value))))
+		Array.from(document.querySelector('#buttons').children).forEach(child => child.addEventListener('click', getSnoozeButtonFunction(new Date(child.value))))
 	}
 
 	// initialize history page
