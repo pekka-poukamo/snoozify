0f0e4a2dfc29c8fa08f81720735701a065ea8918
Pekka Poukamo <p.poukamo@gmail.com>
2023-08-01 12:22:08 +0300

    Add plan for new data structure

--- scripts/storage.js
+++ scripts/storage.js
@@ -2,6 +2,45 @@ import { testing } from '/scripts/testing.js'
 
 const storageKey = testing ? 'snoozedPages_test' : 'snoozedPages'
 
+
+/*
+Plan for new data structure.
+
+Goals:
+- Minimize size of each API call to below 8kb
+- Minimize size of total storage under 102,4kB
+- Provide scalability to dozens/hundreds of sleeping tabs
+- Minimize amount of API calls to under 1800/h and 120/min
+- Minimize the amount of items in db to under 512
+
+snoozify_dates = [
+	'20230816'
+] // array of possible dates, probably isostring
+
+for each date in the list:
+~100B each page for date -> call limit ~80 per date
+snoozify_20230816 = [
+	{
+		page_title: 'Provide scalability to dozens/hundreds of sleeping tabs',
+		page_hash: 123456	
+	}
+] // array of all pages to be woken up on that day
+
+For each page:
+an object to contain all of the relevant metadata 
+(~350B each, total storage supports max ~300 of these objects)
+snoozify_page_123456 = {
+	page_title: 'Provide scalability to dozens/hundreds of sleeping tabs',
+	page_url: 'Provide scalability to dozens/hundreds of sleeping tabs' // Trim URLs to be short?
+	page_hash: 123456,
+	last_wake: date,
+	num_snoozes: 1,
+	snooze_period: 30 days
+} // 
+
+*/
+
+
 const clearSnoozedPages = () => chrome.storage.sync.set({storageKey: []})
 
 const getSnoozedPages = () => {
